e49eaf0e29df298ac2c4609ec1d0072231e97c27
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-02-17 16:19:07 +0600

    basic-js-added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-const incomeField = document.getElementById('income-field');
+// button==========================================================
 const calculateButton = document.getElementById('calculate-button');
+const saveButton = document.getElementById('save-button');
+// field============================================================
+const incomeField = document.getElementById('income-field');
+const foodField = document.getElementById('food-field');
+const rentField = document.getElementById('rent-field');
+const clothField = document.getElementById('cloth-field');
+const saveField = document.getElementById('save-field')
+    // innerText========================================================
+const totalExpenses = document.getElementById('total-expenses');
+const balance = document.getElementById('balance');
+const savingAmount = document.getElementById('saving-amount');
+const remainingBalance = document.getElementById('remaining-balance')
+    // ================================================================
 calculateButton.addEventListener('click', function() {
-    console.log(incomeField.value);
+        totalExpenses.innerText = parseInt(foodField.value) + parseInt(clothField.value) + parseInt(rentField.value);
+        balance.innerText = parseInt(incomeField.value) - parseInt(totalExpenses.innerText);
+    })
+    // =================================================================
+saveButton.addEventListener('click', function() {
+    savingAmount.innerText = parseInt(incomeField.value) * parseInt(saveField.value) * 0.01;
+    remainingBalance.innerText = parseInt(balance.innerText) - parseInt(savingAmount.innerText)
+
 })
