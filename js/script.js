ddff468a99b394318a00d1b55cc4246e325d72d7
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-02-17 16:40:42 +0600

    basic-function-added

--- js/script.js
+++ js/script.js
@@ -6,20 +6,29 @@ const incomeField = document.getElementById('income-field');
 const foodField = document.getElementById('food-field');
 const rentField = document.getElementById('rent-field');
 const clothField = document.getElementById('cloth-field');
-const saveField = document.getElementById('save-field')
-    // innerText========================================================
+const saveField = document.getElementById('save-field');
+// innerText========================================================
 const totalExpenses = document.getElementById('total-expenses');
 const balance = document.getElementById('balance');
 const savingAmount = document.getElementById('saving-amount');
-const remainingBalance = document.getElementById('remaining-balance')
-    // ================================================================
+const remainingBalance = document.getElementById('remaining-balance');
+// ================================================================
 calculateButton.addEventListener('click', function() {
-        totalExpenses.innerText = parseInt(foodField.value) + parseInt(clothField.value) + parseInt(rentField.value);
-        balance.innerText = parseInt(incomeField.value) - parseInt(totalExpenses.innerText);
+        calculateExpenses();
     })
     // =================================================================
 saveButton.addEventListener('click', function() {
-    savingAmount.innerText = parseInt(incomeField.value) * parseInt(saveField.value) * 0.01;
-    remainingBalance.innerText = parseInt(balance.innerText) - parseInt(savingAmount.innerText)
-
+    calculateSavings();
 })
+
+// calculate total expenses and balance
+function calculateExpenses() {
+    totalExpenses.innerText = parseInt(foodField.value) + parseInt(clothField.value) + parseInt(rentField.value);
+    balance.innerText = parseInt(incomeField.value) - parseInt(totalExpenses.innerText);
+}
+
+// calculate savings and remaining balance
+function calculateSavings() {
+    savingAmount.innerText = parseInt(incomeField.value) * parseInt(saveField.value) * 0.01;
+    remainingBalance.innerText = parseInt(balance.innerText) - parseInt(savingAmount.innerText);
+}
