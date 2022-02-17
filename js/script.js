e34c18d0a23e08b11241ecfaf6bd82445ceb772b
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-02-17 15:41:25 +0600

    page-design-complete

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const incomeField = document.getElementById('income-field');
+const calculateButton = document.getElementById('calculate-button');
+calculateButton.addEventListener('click', function() {
+    console.log(incomeField.value);
+})
